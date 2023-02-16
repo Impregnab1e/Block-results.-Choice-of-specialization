48a3f8e7736b0e0a7e98b243048cd0232a14325d
Dmitriy <wuwka1@ya.ru>
2023-02-16 22:01:54 +0300

    add program for task. Add x2 Method - GetDataFromUser and ArrayFromUser

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿/*Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
+При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.*/
+
+Console.Clear();
+
+int GetDataFromUser(string message)
+{
+    Console.WriteLine(message);
+    int result = int.Parse(Console.ReadLine()!);
+    return result;
+}
+
+
+string[] ArrayFromUser(int length)
+{
+    string[] array = new string[length];
+    for (int i = 0; i < length; i++)
+    {
+        Console.WriteLine("Введите данные: ");
+        array[i] = Convert.ToString(Console.ReadLine());
+    }
+    return array;
+}
+
